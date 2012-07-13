ee0f008e66718860507fca536f410cfadc2f9593
lg2046 <lg2046@gmail.com>
2012-07-13 08:46:08 +0800

    bump 0.2.2

--- lib/rails_extend/active_support/all.rb
+++ lib/rails_extend/active_support/all.rb
@@ -1,4 +1,4 @@
 Dir.glob(File.expand_path(File.dirname(__FILE__) + '/*.rb')) do |file|
-  next if file.ends_withs("all.rb")
+  next if file.ends_withs?("all.rb")
   require File.expand_path(File.dirname(__FILE__)) + "/" + File.basename(file, ".rb")
 end
