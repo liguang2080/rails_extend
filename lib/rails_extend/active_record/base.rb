3c73c8f8c385e52cc98f6f55c3c69f6d6bf2b42b
lg2046 <lg2046@gmail.com>
2012-11-06 10:47:42 +0800

    grammer fix

--- lib/rails_extend/active_record/base.rb
+++ lib/rails_extend/active_record/base.rb
@@ -13,7 +13,7 @@ module ActiveRecord
 
     def dom_id(prefix='')
       display_id = new_record? ? "new" : id.to_s
-      prefix.to_s <<( '_') unless prefix.blank?
+      prefix.to_s << ('_') unless prefix.blank?
       prefix.to_s << "#{self.class.name.underscore.gsub('/', '_')}"
       prefix != :bare ? "#{prefix.to_s.underscore}_#{display_id}" : display_id
     end
