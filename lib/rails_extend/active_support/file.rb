054ae88b823f1a87c41edb80c5fc19eab63aab6e
lg2046 <lg2046@gmail.com>
2012-12-02 17:58:31 +0800

    add new upload

--- lib/rails_extend/active_support/file.rb
+++ lib/rails_extend/active_support/file.rb
@@ -14,4 +14,19 @@ class File
       return tmp_path
     end
   end
+  
+  def self.new_upload(upload_file)
+    tmp_path  = Rails.root.to_s + "/tmp/#{RandomCode.mix_string(16)}#{File.extname(url)}"
+    File.open(tmp_path, "wb") { |f| f.write(params[:image].read) }
+    
+    if block_given?
+      begin
+        yield tmp_path
+      ensure
+        FileUtils.rm([tmp_path])
+      end
+    else
+      return tmp_path
+    end
+  end
 end
